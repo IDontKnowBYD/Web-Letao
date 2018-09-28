86c0e839ac5f2e7907fff70951e87f881fc4c4d9
zhushaofeng <zhushaofeng19960409@gmail.com>
2018-09-28 10:10:55 +0800

    分页功能完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,15 +1,42 @@
 $(function(){
-  $.ajax({
-    type: 'get',
-    url: '/user/queryUser',
-    data: {
-      page: 1,
-      pageSize: 5
-    },
-    dataType: 'json',
-    success: function(info) {
-      var tmp = template("tmp", info);
-      $('tbody').html( tmp );
-    }
-  })
+
+  var currentPage = 1;
+  var pageSize = 5;
+  var totalPage = 0;
+
+  function render(cP, pS) {
+    $.ajax({
+      type: 'get',
+      url: '/user/queryUser',
+      data: {
+        page: cP,
+        pageSize: pS
+      },
+      dataType: 'json',
+      success: function (info) {
+        console.log(info);
+        
+        var tmp = template("tmp", info);
+        $('tbody').html(tmp);
+        totalPage = Math.ceil(info.total / info.size);
+
+        setPage(currentPage, totalPage);
+      }
+    })
+  }
+
+  function setPage(cP, tP) {
+    $("#pagintor").bootstrapPaginator({
+      bootstrapMajorVersion: 3,
+      currentPage: cP,
+      totalPages: tP,
+      size: "small",
+      onPageClicked: function (event, originalEvent, type, page) {
+        currentPage = page;
+        render(currentPage, pageSize);
+      }
+    });
+  }
+
+  render(currentPage, pageSize);
 })
