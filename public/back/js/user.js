c46d63ad97fbdffa20d3fc9a03b6122c4965e4ac
zhushaofeng <zhushaofeng19960409@gmail.com>
2018-09-28 16:04:32 +0800

    用户-功能完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -3,7 +3,7 @@ $(function(){
   var currentPage = 1;
   var pageSize = 5;
   var totalPage = 0;
-
+  //渲染
   function render(cP, pS) {
     $.ajax({
       type: 'get',
@@ -14,7 +14,6 @@ $(function(){
       },
       dataType: 'json',
       success: function (info) {
-        console.log(info);
         
         var tmp = template("tmp", info);
         $('tbody').html(tmp);
@@ -39,4 +38,30 @@ $(function(){
   }
 
   render(currentPage, pageSize);
+
+  $('.container-fluid .table tbody').on('click', '.btn', function() {
+    $('#userModal').modal('show');
+
+    var id = $(this).parent().data('id');
+    var isDelete = $(this).hasClass('btn-success') ? 1 : 0;   
+    
+    $('.userBtn').off('click').on('click', function(){
+      $.ajax({
+        type: 'post',
+        url: '/user/updateUser',
+        data: {
+          id: id,
+          isDelete: isDelete
+        },
+        success: function (info) {
+         if(info.success){
+          $('#userModal').modal("hide");
+          render(currentPage, pageSize);
+         }
+        }
+      })
+    })
+  })
+
+  
 })
