1b470ee27bdfe017dac837ef2fec0885cce1a37b
zhushaofeng <zhushaofeng19960409@gmail.com>
2018-10-08 10:59:04 +0800

    商品-分类，图片上传完成

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -3,6 +3,8 @@ $(function () {
   var pageSize = 5;
   var totalPage = 0;
 
+  var picArr = [];
+
   //渲染
   function render(cP, pS) {
     $.ajax({
@@ -34,7 +36,39 @@ $(function () {
       onPageClicked: function (event, originalEvent, type, page) {
         currentPage = page;
         render(currentPage, pageSize);
-      }
+      },
+      //控制按钮显示的文字
+      itemTexts: function (type, page, current) {
+        switch (type) {
+          case "page":
+            return page;
+          case "first":
+            return "首页";
+          case "last":
+            return "尾页";
+          case "prev":
+            return "上一页";
+          case "next":
+            return "下一页";
+        }
+      },
+      // 将该函数的返回值, 作为按钮的 title 提示文本
+      tooltipTitles: function (type, page, current) {
+        switch (type) {
+          case "page":
+            return "前往第" + page + "页";
+          case "first":
+            return "首页";
+          case "last":
+            return "尾页";
+          case "prev":
+            return "上一页";
+          case "next":
+            return "下一页";
+        }
+      },
+      //使用 bootstrap 的提示框组件
+      useBootstrapTooltip: true
     });
   }
 
@@ -43,5 +77,48 @@ $(function () {
   // 打开模态框
   $('.addPro').click(function () {
     $('#addModal').modal('show');
+    //添加分类
+    $.ajax({
+      type: "get",
+      url: "/category/querySecondCategoryPaging",
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: "json",
+      success: function (info) {
+        console.log(info)
+        var htmlStr = template("dropdownTpl", info);
+        $(".dropdown-menu").html(htmlStr);
+      }
+    })
+  })
+
+  //下拉菜单
+  $('.dropdown-menu').on("click", "a", function () {
+    var txt = $(this).text();
+    $('#dropdownTxt').text(txt);
+
+    var id = $(this).data("id");
+    $('[name="brandId"]').val(id);
+
+  });
+
+  //图片上传
+  $('#fileupload').fileupload({
+    dataType: "json",
+    done: function (e, data) {
+
+      picArr.unshift(data.result);
+
+      var picUrl = data.result.picAddr;
+
+      $('#imgBox').prepend('<img src="' + picUrl + '" width="100" height="100" alt="">');
+
+      if (picArr.length > 3) {
+        $('#imgBox img:last-of-type').remove();
+        picArr.pop();
+      }
+    }
   })
 })
